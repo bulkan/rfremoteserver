a78a333d543cddab77600ed377734f6943f88931
Bulkan Evcimen <bulkan@gmail.com>
2013-09-01 00:30:49 +1000

    added test keyword that uses async function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var util         = require('util')
+  , fs           = require('fs')
   , RemoteServer = require('./lib/remoteserver');
 
 var TestLibrary = function() {
@@ -10,7 +11,17 @@ var TestLibrary = function() {
       return: 'return value of keyword'
     }
     return this.pass(ret, callback);
-  }
+  };
+
+  self.get_files_in_directory = function(params, callback){
+    var self = this;
+    var ret = {}
+    fs.readdir('.', function(err, files){
+      ret.return = err || files;
+      if (err) return self.fail(ret, callback);
+      return self.pass(ret, callback)
+    });
+  };
 };
 
 util.inherits(TestLibrary, RemoteServer);
