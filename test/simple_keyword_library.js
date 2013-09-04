94207ecca08d23c29593168f5477371807879d2e
Bulkan Evcimen <bulkan@gmail.com>
2013-09-05 08:52:27 +1000

    tests that we can run the keywords through xmlrpc
    fixed an undefined variable in the fail method
    using .shift on array

--- test/simple_keyword_library.js
+++ test/simple_keyword_library.js
@@ -9,14 +9,19 @@ var options = {host: 'localhost', port: 8270};
 var SimpleKeywordLibrary = function(conf) {
   var self = this;
   self.conf = conf;
-  
-  self.file_should_exist = function(dir, file, callback) {
+
+  self.file_should_exist = function(params, callback) {
+    var dir = params.shift();
+    var file = params.shift();
+    var ret = {};
     fs.readdir(dir, function(err, files){
       ret.return = err || files;
       if (err) return self.fail(ret, callback);
 
-      if (file in files)
-        return self.pass(ret, callback);
+      for (i in files){
+        if (files[i] == file)
+          return self.pass(ret, callback);
+      }
       return self.fail(ret, callback);
     });
   };
@@ -49,6 +54,18 @@ describe('SimpleKeywordLibrary', function(){
 
       value.should.not.be.empty;
       value.should.include('file should exist');
+      return done();
+    });
+  });
+
+  it('should be able to run keywords', function(done){
+    var client = new xmlrpc.createClient(options, false);
+    client.methodCall('run_keyword', ['file should exist', '.', 'package.json'], function(err, value){
+      if (err) return done(err);
+      value.should.have.property('return');
+      value.should.have.property('status');
+      value.return.should.include('lib');
+      value.status.should.be.equal('PASS');
       return done();
     });
   });
