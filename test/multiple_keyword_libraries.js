5efe333f2d4b716df7707a7e49d15bf3d88fcdc0
Bulkan Evcimen <bulkan@gmail.com>
2013-09-12 08:14:51 +1000

    test if remoteserver can load multiple libraries

--- test/multiple_keyword_libraries.js
+++ test/multiple_keyword_libraries.js
@@ -1,22 +1,43 @@
-var xmlrpc       = require('xmlrpc')
-  , fs           = require('fs')
-  , RemoteServer = require('../lib/remoteserver')
+var xmlrpc               = require('xmlrpc')
+  , fs                   = require('fs')
+  , RemoteServer         = require('../lib/remoteserver')
   , libraries            = require('./keyword_libraries')
-  , SimpleKeywordLibrary = libraries.SimpleKeywordLibrary;
+  , SimpleKeywordLibrary = libraries.SimpleKeywordLibrary
+  , FileSystemLibrary    = libraries.FileSystemLibrary;
 
 var options = {host: 'localhost', port: 4242};
 
 describe('Multiple Keyword Libraries', function(){
   var server = null;
+  var client = null;
 
   before(function(done){
-    server = new RemoteServer(options, [SimpleKeywordLibrary]);
+    server = new RemoteServer(options, [SimpleKeywordLibrary, FileSystemLibrary]);
     server.start_remote_server();
-    done();
+    // need to give the server a litte time to start
+    setTimeout(function() { 
+      client = new xmlrpc.createClient(options, false);
+      done();
+    }, 100);
   });
 
   after(function(done){
     server.close(done);
   });
 
+  it('should be able to be loaded', function(done){
+    client.methodCall('get_keyword_names', null, function(err, value){
+      if (err) return done(err);
+
+      value.should.not.be.empty;
+      value.should.include('get keyword documentation');
+      value.should.include('get keyword names');
+      value.should.include('get keyword arguments');
+      value.should.include('dir should exist');
+      value.should.include('run keyword');
+      done();
+    });
+  });
+
 });
+
