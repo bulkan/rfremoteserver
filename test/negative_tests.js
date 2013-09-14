8e0039937aebddd7ea9d4e30da5700f86cbcf197
Bulkan Evcimen <bulkan@gmail.com>
2013-09-14 22:31:48 +1000

    added a few more negative tests
    put traceback into return value when a keyword throws an Error

--- test/negative_tests.js
+++ test/negative_tests.js
@@ -24,9 +24,33 @@ describe('Broken & Bad Keywords', function(){
   });
 
   it('status should be automatically be set to FAIL', function(done){
+    client.methodCall('run_keyword', ['get error value'], function(err, value){
+      if (err) return done(err);
+      value.should.have.property('error');
+      value.status.should.be.equal('FAIL');
+      done();
+    });
+  });
+
+  it('traceback should be set', function(done){
     client.methodCall('run_keyword', ['throw an error'], function(err, value){
       if (err) return done(err);
       value.should.have.property('error');
+      value.should.have.property('traceback');
+      value.status.should.be.equal('FAIL');
+      value.traceback.should.not.be.empty;
+      done();
+    });
+  });
+
+  it('try to run a nonexisting keyword', function(done){
+    client.methodCall('run_keyword', ['get something'], function(err, value){
+      if (err) return done(err)
+      decodeURIComponent(value.traceback).should.include('Error: keyword get_something not found');
+      value.should.have.property('error');
+      value.should.have.property('traceback');
+      value.status.should.be.equal('FAIL');
+      value.traceback.should.not.be.empty;
       done();
     });
   });
